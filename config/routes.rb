ffab05b3f7c4d71d2e65f167225e8191685e8602
Wongsathorn Panichkurkul <wongsathorn.pa@ku.th>
2017-05-25 09:07:38 +0000

    Update Admin page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
 
 get '/help', to: 'static_pages#help'
 get '/home' , to: 'static_pages#home'
+get '/adminBoard' ,to:'static_pages#Administration.BoardofAdministrations'
+get 'adminDepartment' ,to:'static_pages#Administration.DepartmentHead'
+get 'adminFaculty', to:'static_pages#Administration.FacultyMembers'
+get 'adminSenior' ,to:'static_pages#Administration.Seniors&ExecutiveMembers'
+
 
 end
