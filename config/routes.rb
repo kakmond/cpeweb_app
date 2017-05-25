61d2321f7cfc6fd2785564d44e20dd9e2a13d8e7
Wongsathorn Panichkurkul <wongsathorn.pa@ku.th>
2017-05-25 07:12:09 +0000

    Add menubar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/help'
-
-root 'application#hello'
+get '/help', to: 'static_pages#help'
+get '/home' , to: 'static_pages#home'
 
 end
